ddb0f3888cac31ac519e7dc1ef4b99ac10f988e8
Kristian Rasmussen <me@krx.io>
2019-11-22 09:56:58 +0100

    adding table name and fetch

--- templates/Java - JDBI DAO.java
+++ templates/Java - JDBI DAO.java
@@ -1,3 +1,6 @@
+#set( $regex = "([a-z])([A-Z]+)")
+#set( $replacement = "$1_$2")
+#set( $toLowDash = $NAME.replaceAll($regex, $replacement).toLowerCase())
 package ${PACKAGE_NAME};
 
 import org.skife.jdbi.v2.sqlobject.*;
@@ -8,4 +11,9 @@ import org.skife.jdbi.v2.unstable.BindIn;
 @UseStringTemplate3StatementLocator
 public interface ${NAME}Dao {
     static ${NAME}Dao getInstance() { return JdbiFactory.getDataSource("${Datasource_name}").onDemand(${NAME}Dao.class); }
+    
+    @SqlQuery("SELECT * FROM ${toLowDash} WHERE id = :id")
+    @Mapper(${NAME}Mapper.class)
+    ${NAME}Entity fetch(@Bind("id") long id);
+    
 }
